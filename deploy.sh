6898bc09a1d21c7eb4279e62b28d98422e73bb2a
kamikasu <kamikasu620@gmail.com>
2021-12-08 20:51:30 -0500

    deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,5 @@
 #!/bin/sh
 
-# echo "Kill all the running PM2 actions"
-# sudo pm2 kill
-
-# echo "Jump to app folder"
-# cd /home/ubuntu/app-name
-
-# echo "Update app from Git"
-# git pull
-
-# echo "Install app dependencies"
-# sudo rm -rf node_modules package-lock.json
-# sudo npm install
-
-# echo "Build your app"
-# sudo npm run build
-
-# echo "Run new PM2 action"
-# sudo cp /home/ubuntu/ecosystem.json ecosystem.json
-# sudo pm2 start ecosystem.json
-
-
-
 mkdir -p /home/ubuntu/app/
 
 rm -rf /home/ubuntu/app/*
